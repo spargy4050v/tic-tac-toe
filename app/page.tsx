d3881bbf23bffe0336bb38f6a3b3bf3421f83b66
Rishith Kumar Guntuka <spargy4050v@gmail.com>
2025-05-15 13:36:12 +0530

    First One

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@
 import { useState } from 'react';
 import Image from "next/image";
 
-function Square({value, onSquareClick}) {
+type SquareProps = {
+  value: string | null;
+  onSquareClick: () => void;
+}
+
+function Square({value, onSquareClick}: SquareProps) {
   return (
     <button
       className="square"
@@ -40,28 +45,30 @@ export default function Board() {
   }
   
   return (
-  <div className="board">
-    <div className="flex flex-row">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-    </div>
-    <div className="flex flex-row">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-    </div>
-    <div className="flex flex-row">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-    </div>
-    <div className="status">{status}</div>
-  </div>
+    <>
+      <div className="board">
+        <div className="flex flex-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="flex flex-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="flex flex-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </div>
+      <div className="status">{status}</div>
+    </>
 );
 }
 
-function calculateWinner(squares) {
+function calculateWinner(squares: (string | null)[]): string | null {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
